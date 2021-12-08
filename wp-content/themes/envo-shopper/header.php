c5694861e8df34938fb94712378fc0cd31443f14
yentran021201 <68818558+yentran021201@users.noreply.github.com>
2021-12-08 15:45:20 +0700

    Add css footer link.

--- wp-content/themes/envo-shopper/header.php
+++ wp-content/themes/envo-shopper/header.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <?php wp_head(); ?>
+        <!-- css footer -->
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/footer.css" type="text/css" media="screen" />
     </head>
     <body id="blog" <?php body_class(); ?>>
         <?php wp_body_open(); ?>
